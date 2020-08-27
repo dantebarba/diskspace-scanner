f3043d6dc3d3c63ee6f0a368905b6130cd3b826c
Dante Barba <dantebarba@gmail.com>
2020-08-27 01:26:12 -0300

    docker ignore and building system improved with versioning.

--- README.md
+++ README.md
@@ -4,4 +4,9 @@ Python based scanner app that tracks disk space usage.
 
 # Usage
 
+1. Run ./build.sh $(git describe --tags --dirty --always) for building the image snapshot.
+2. Then run docker-compose up to test the container.
+
+
+
 
--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker build --pull --build-arg BUILD_VERSION=$(git describe --tags --dirty --always) --rm -f "Dockerfile" -t dantebarba/diskspacescanner:latest "."
+docker build --pull --build-arg BUILD_VERSION=$(git describe --tags --dirty --always) --rm -f "Dockerfile" -t dantebarba/diskspacescanner:$@ "."
